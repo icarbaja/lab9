a42b1cc2b21b7e432b320ce8c99884cb7e2f5939
Ivan Carbajal <icarbaja@uncc.edu>
2019-09-12 14:15:10 +0000

    Lab 8 initial commit

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -2,14 +2,19 @@
 # Part I
 def array_2_hash emails, contacts
     # YOUR CODE HERE
+    if emails.length == 0
+        return contacts
+    else
+        x = 0
+        keyarray = contacts.keys
+        hash = Hash.new
+        keyarray.each do |y|
+            hash[y] = emails[x]
+            x = x + 1
+        end
+    end
+    return hash
 end
 
-# Part II
-def array2d_2_hash contact_info, contacts
-    # YOUR CODE HERE
-end
 
-# Part III
-def hash_2_array contacts
-    # YOUR CODE HERE
-end
+
