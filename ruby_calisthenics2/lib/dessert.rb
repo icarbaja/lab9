d3bc1abda7db4107c5caebdde953d6ccaa49ea97
Ivan Carbajal <icarbaja@uncc.edu>
2019-09-26 13:05:56 +0000

    Lab 11 initial commit

--- ruby_calisthenics2/lib/dessert.rb
+++ ruby_calisthenics2/lib/dessert.rb
@@ -1,19 +1,37 @@
 class Dessert
-  # add code for setters and getters
+  attr_accessor :name
+  attr_accessor :calories
+  
   def initialize(name, calories)
-    # your code here
+    @name=name
+    @calories=calories
   end
   def healthy?
     # your code here
+      if(calories < 200)
+        return true
+       end
+    return false
   end
+  
   def delicious?
     # your code here
+    if(@flavor == "licorice")
+      return false
+    end
+    return true
   end
 end
 
 class JellyBean < Dessert
-  # add code for setters and getters
-  def initialize(flavor)
-    # your code here
-  end
+
+attr_accessor :flavor
+
+def initialize(flavor)
+     @flavor = flavor 
+     @calories = 5
+    @name = flavor + " jelly bean"
+   
+   
+end
 end
